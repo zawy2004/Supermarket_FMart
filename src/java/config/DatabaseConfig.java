80bb812b951cef6f49e4cb7a4cda8620395845c7
zawy2004 <161670490+zawy2004@users.noreply.github.com>
2025-06-13 21:36:56 +0700

    database ne may con cho

--- src/java/config/DatabaseConfig.java
+++ src/java/config/DatabaseConfig.java
@@ -4,10 +4,26 @@
  */
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author thanh
  */
 public class DatabaseConfig {
+    private static final String URL = "jdbc:sqlserver://zawy-laptop;databaseName=Store;encrypt=false";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "1234";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("JDBC Driver not found", e);
+        }
+    }
     
 }
